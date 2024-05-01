d2dc866a66d7fe3d9a4a1f5b42325e4e61d512e5
arpit <mayanksingh7382@gmail.com>
2024-05-01 10:27:36 +0530

    RECENTY 30/04/2024 add

---
+++ src/POLY_MANY/STUDENT.java
@@ -0,0 +1,15 @@
+package POLY_MANY;
+
+public class STUDENT    {
+
+      public void reading(){
+
+         System.out.println(" student is reading a book of OOPS JAVA CONCEPT");
+
+     }
+     // having a different parameter with the same name is overloading
+   public void reading(String bookname){
+        System.out.println("READDING "+bookname+" regular");
+
+    }
+}
---
+++ src/POLY_MANY/demo2.java
@@ -0,0 +1,17 @@
+package POLY_MANY;
+
+public class demo2 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("this just for the testing");
+        // ALWAYS CREATE AND OBJECT OF THE ENTITY CLASS
+
+        STUDENT s1=new STUDENT();
+
+       // s1.reading();
+        s1.reading("lets have full stack php");// compile time polymorphism
+    }
+
+}
